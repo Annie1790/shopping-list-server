1c45db9d70a1118093b06131d3cc00e388004c0f
Annie1790 <budayaniko@outlook.com>
2023-06-05 16:31:55 +0100

    POST added to shopItemRouter with test json database

--- routes/shopItem.js
+++ routes/shopItem.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const fs = require("fs");
+const resolve = require("path").resolve;
+let id = 0;
 
 shopItemRouter = express.Router();
 
-shopItemRouter.post("/", (req,res,next) => {
+shopItemRouter.use("/", (req,res,next) => {
+id++;
+req.body.id = id;
+next();
+})
 
+shopItemRouter.post("/", (req, res, next) => {
+    res.json(`${req.method} success`);
+    console.log(req.body);
+    fs.writeFileSync(resolve("./database.json"), JSON.stringify(req.body) + "\n", { flag: "a+" }, error => { console.log(error) })
 });
 
-shopItemRouter.put("/", (req,res,next) => {
+shopItemRouter.put("/", (req, res, next) => {
 
 });
 
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require("cors");
 const PORT = 4000;
 
+app.use(cors());
+app.use(express.json());
+
 const shopItemRouter = require("./routes/shopItem.js");
 app.use("/shopItem", shopItemRouter);
 
@@ -11,8 +14,6 @@ app.use("/shopItem/:id", findIdRouter);
 
 const tickStatusRouter = require("./routes/tickStatus.js");
 app.use("/shopItem/findByTickStatus", tickStatusRouter);
-
-app.use(cors);
 
 app.use((req,res,next) => {
     console.log(req.get("user-agent"));
