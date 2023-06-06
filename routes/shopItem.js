87414cea6934e6b0334b697696b4794c6e36cf3d
Annie1790 <budayaniko@outlook.com>
2023-06-06 12:36:14 +0100

    pull request

--- routes/shopItem.js
+++ routes/shopItem.js
@@ -7,14 +7,16 @@ shopItemRouter = express.Router();
 
 shopItemRouter.post("/", (req, res, next) => {
     try {
-        if ("isCompleted" in req.body === false) {
-            req.body.isCompleted = false;
-        };
-        id++;
-        req.body.id = id;
-        fs.writeFileSync(resolve("./database.json"), JSON.stringify(req.body) + "\n", { flag: "a+" });
-        res.json(`${req.method} success`);
-        res.status(201).send();
+        if (typeof(req.body.name) === "string" && typeof(req.body.isCompleted === "boolean")) {
+            if ("isCompleted" in req.body === false) {
+                req.body.isCompleted = false;
+            };
+            id++;
+            req.body.id = id;
+            fs.writeFileSync(resolve("./database.json"), JSON.stringify(req.body) + "\n", { flag: "a+" });
+            res.json(`${req.method} success`);
+            res.status(201).send();
+        }
     }
     catch (error) {
         console.log(error);
