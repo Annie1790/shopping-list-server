02739f0d9e9f49f04cdb4c3fe2d55b60875526fc
Annie1790 <budayaniko@outlook.com>
2023-06-22 17:45:55 +0100

    new solution

--- routes/shopItemByStatus.js
+++ routes/shopItemByStatus.js
@@ -26,27 +26,25 @@ findStatusRouter.use(utility.readDatabaseMiddleware);
 
 //My solution with sqlite
 findStatusRouter.get("/", (req, res, next) => {
-    const result = db.all("SELECT rowid, * FROM grocery_list", (err, row) => {
+    db.all("SELECT rowid, * FROM grocery_list", (err, row) => {
         if (err) {
             throw err
         } else {
-            console.log(row);
-            return row;
+            const isCompleted = req.query.isCompleted || "";
+            if (isCompleted === "true" || isCompleted === "false" || isCompleted === "") {
+                if (isCompleted === "true") {
+                    row = utility.filterByStatus(row, Number(1))
+                } else if (isCompleted === "false") {
+                    row = utility.filterByStatus(row, Number(0))
+                }
+                res.status(200);
+                res.send(row);
+            } else {
+                res.status(400).send();
+            }
         }
     });
-    const isCompleted = req.query.isCompleted || "";
-    let groceryArray = result;
-    if (isCompleted === "true" || isCompleted === "false" || isCompleted === "") {
-        if (isCompleted === "true") {
-            groceryArray = utility.filterByStatus(groceryArray, 1)
-        } else if (isCompleted === "false") {
-            groceryArray = utility.filterByStatus(groceryArray, 0)
-        }
-        res.status(200);
-        res.send(groceryArray);
-    } else {
-        res.status(400).send();
-    }
+
 
 })
 
